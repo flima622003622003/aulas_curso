41e9b5fe4c188af02bf4e2133b1e9d69eb57a680
Fabio Lima <flima622003@gmail.com>
2022-04-29 12:40:44 -0300

    if else

--- tarefa3/exe3.js
+++ tarefa3/exe3.js
@@ -2,7 +2,7 @@ let produtoA = {"nome": "camiseta", "valor": 100.00, "internacional": true};
 let produtoB = {"nome": "perfume", "valor": 200.00, "internacional": true};
 let produtoC = {"nome": "sandália", "valor": 120.00, "internacional": false};
 
-//CONDICIONAIS
+// CONDICIONAIS if else
 
 if (produtoA.internacional == true) {
    let valorTaxasA = produtoA.valor * 1.2
